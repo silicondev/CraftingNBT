53c553530cb956f48ee0eb31767fda2a71cb2f72
silicondev <47757443+silicondev@users.noreply.github.com>
2020-03-25 01:30:16 +0000

    v0.0.4 RELEASE v0.1
    Refactored code and added furnace support.

--- src/main/java/io/github/silicondev/craftingnbt/listeners/CraftListener.java
+++ src/main/java/io/github/silicondev/craftingnbt/listeners/CraftListener.java
@@ -4,13 +4,17 @@ import java.util.Map;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.FurnaceSmeltEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
@@ -30,32 +34,55 @@ public class CraftListener implements Listener {
 	
 	@EventHandler
 	public void onCraftItem(PrepareItemCraftEvent event) {
+		boolean debug = _config.getBoolean("debug");
+		
 		if (event.getRecipe() == null)
 			return;
-		
 		ItemStack item = event.getRecipe().getResult();
+		
 		Material type = item.getType();
 		if (!type.isBlock())
 			return;
 		
+		Player player = (Player)event.getInventory().getHolder();
+
+		event.getInventory().setResult(_modifyItem(player, player.getWorld(), item, debug));
+	}
+	
+	@EventHandler
+	public void onSmeltItem(FurnaceSmeltEvent event) {
 		boolean debug = _config.getBoolean("debug");
 		
-		_runCommand("scoreboard objectives add tempStore dummy");
+		if (event.getResult() == null)
+			return;
+		ItemStack item = event.getResult();
 		
-		Player player = (Player)event.getInventory().getHolder();
+		Material type = item.getType();
+		if (!type.isBlock())
+			return;
+		
+		
+		event.setResult(_modifyItem(_parent.getServer().getConsoleSender(), event.getBlock().getWorld(), item, debug));
+	}
+	
+	private ItemStack _modifyItem(CommandSender sender, World world, ItemStack item, boolean debug) {
+		
+		ItemStack result = item;
 		
 		Scoreboard scoreboard = _parent.getServer().getScoreboardManager().getMainScoreboard();
+		if (scoreboard.getObjective("tempStore") == null)
+			scoreboard.registerNewObjective("tempStore", "dummy", "tempStore");
 		Objective objective = scoreboard.getObjective("tempStore");
-		if (debug) player.sendMessage("[CraftingNBT/debug] Added tempStore Objective.");
 		
-		Location loc = new Location(player.getWorld(), 0, 0, 0);
-		loc.getBlock().setType(type);
-		if (debug) player.sendMessage("[CraftingNBT/debug] Set block to one that's being crafted.");
+		if (debug) sender.sendMessage("[CraftingNBT/debug] Added tempStore Objective.");
 		
-		ConfigurationSection section = _config.getConfigurationSection("nbt");
-		Map<String, Object> nodes = section.getValues(false);
+		Location loc = new Location(world, 0, 0, 0);
+		loc.getBlock().setType(item.getType());
+		if (debug) sender.sendMessage("[CraftingNBT/debug] Set block to one that's being crafted.");
 		
-		if (debug) player.sendMessage("[CraftingNBT/debug] Running through nbt nodes.");
+		Map<String, Object> nodes = _config.getConfigurationSection("nbt").getValues(false);
+		
+		if (debug) sender.sendMessage("[CraftingNBT/debug] Running through nbt nodes.");
 		for (Map.Entry<String, Object> entry : nodes.entrySet()) {
 			if (entry.getValue() instanceof ConfigurationSection) {}
 			else 
@@ -66,40 +93,35 @@ public class CraftListener implements Listener {
 			String tag = subSection.getString("tag").trim();
 			String nbt = subSection.getString("data").trim();
 			
-			if (debug) player.sendMessage("[CraftingNBT/debug] Checking " + tag + ".");
+			if (debug) sender.sendMessage("[CraftingNBT/debug] Checking " + tag + ".");
 			
 			if (!tag.startsWith("#"))
 				tag = "#" + tag;
 			
-			_runCommand("execute store success score " + player.getName() + " tempStore if block 0 0 0 " + tag);
+			String execute = "execute store success score " + sender.getName() + " tempStore if block 0 0 0 " + tag;
+			_parent.getServer().dispatchCommand(sender, execute);
 			
-			int score = objective.getScore(player.getName()).getScore();
+			int score = objective.getScore(sender.getName()).getScore();
 			
 			if (score == 1) {
-				if (debug) player.sendMessage("[CraftingNBT/debug] Found matching node and adding " + nbt + ".");
+				if (debug) sender.sendMessage("[CraftingNBT/debug] Found matching node and adding " + nbt + ".");
 				
 				NBTItem nbtItem = new NBTItem(item);
 				nbtItem.mergeCompound(new NBTContainer(nbt));
-				ItemStack newItem = nbtItem.getItem();
+				ItemMeta newItemMeta = nbtItem.getItem().getItemMeta();
 				
-				if (debug && newItem == item) player.sendMessage("[WARN][CraftingNBT/debug] Both new and old items are identical!!");
-				
-				ItemStack tmp = new ItemStack(Material.STONE);
-				ItemStack result = new ItemStack(item.getType());
+				result = new ItemStack(item.getType());
 				result.setAmount(item.getAmount());
-				result.setItemMeta(newItem.getItemMeta());
-				event.getInventory().setResult(result);
+				result.setItemMeta(newItemMeta);
 			}
 		}
 		
 		loc.getBlock().setType(Material.BEDROCK);
-		if (debug) player.sendMessage("[CraftingNBT/debug] Reset block.");
+		if (debug) sender.sendMessage("[CraftingNBT/debug] Reset block.");
 		
 		objective.unregister();
-		if (debug) player.sendMessage("[CraftingNBT/debug] Removed tempStore Objective.");
-	}
-	
-	private void _runCommand(String command) {
-		_parent.getServer().dispatchCommand(_parent.getServer().getConsoleSender(), command);
+		if (debug) sender.sendMessage("[CraftingNBT/debug] Removed tempStore Objective.");
+		
+		return result;
 	}
 }
